57a692e277704d18d9524b4be8629119790eb49b
filterlesschris <c.benzen@yahoo.com>
2018-03-06 21:17:02 -0800

    Set default layer values, drop unwanted layers, etc..

--- js/layers.js
+++ js/layers.js
@@ -17,7 +17,7 @@ var map = L.map("map", {
 var apiKey = "access_token=pk.eyJ1IjoiYWJmZGF0YSIsImEiOiJjamU2aHlrZTgwMGdxMzNxa3R3OG5wZmNkIn0._No3joCSQ0ZhN2KE30LC8w";
 
 // Add default layer
-L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" + apiKey).addTo(map);
+L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?" + apiKey).addTo(map);
 
 // Outdoors - default map
 var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" + apiKey, 
@@ -27,16 +27,16 @@ var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10
 var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/256/{z}/{x}/{y}?" + apiKey,
 {id: 'map'});
 
-// Grayscale map
-var grayscale = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?" + apiKey,
+// dark map
+var dark = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?" + apiKey,
 {id: 'map'});
 
-// Grayscale.addTo(map);
+// dark.addTo(map);
 
 var baseMaps = {
     "Outdoors": outdoors,
     "Satellite": satellite,
-    "Grayscale": grayscale
+    "Dark": dark
 };
 
 // earthquakes all week data
@@ -57,14 +57,6 @@ var controlLayers = L.control.layers(baseMaps).addTo(map);
      weight: 0.5
     };
  };
-
-// COLORS 2:
-// #DC143C crimson      
-// #FF8C00 darkorange      
-// #00CED1 darkturquoise        
-// #9400D3 darkviolet 
-//  #1E90FF dodgerblue
-// #B0E0E6 powderblue  
 
  function getColor(magnitude){
     switch(true) {
@@ -128,40 +120,46 @@ function popPlate(feature, layer) {
 };
 
 // Here we add a GeoJSON layer to the map once the file is loaded.
-d3.json(link, function(data){
-    var earthquakeLayer = L.geoJson(data, {
-        // We turn each feature into a circleMarker on the map.
-        pointToLayer: function(feature, latlng) {
-          return L.circleMarker(latlng);
-        },
-        // We set the style for each circleMarker using our styleInfo function.
-        style: styleInfo,
-        // We create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
-        onEachFeature: populateInfo,
-        
-        
-      }).addTo(map);
-      controlLayers.addOverlay(earthquakeLayer, 'Earthquakes');
-
-    });
+//d3.json(link, function(data){
+//    var earthquakeLayer = L.geoJson(data, {
+//        // We turn each feature into a circleMarker on the map.
+//        pointToLayer: function(feature, latlng) {
+//          return L.circleMarker(latlng);
+//        },
+//        // We set the style for each circleMarker using our styleInfo function.
+//        style: styleInfo,
+//        // We create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
+//        onEachFeature: populateInfo,
+//        
+ //       
+//      }).addTo(map);
+//      controlLayers.addOverlay(earthquakeLayer, 'Earthquakes');
+//
+//    });
 
 borderslink = "https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json";        
 // Grabbing our GeoJSON data..
 d3.json(borderslink, function(data) {
   var bordersLayer = L.geoJson(data, {
+    style: {
+      // Border color
+      color: "#fff",
+      weight: 1,
+      fillOpacity: 0.8
+    },
     }).addTo(map);
-    controlLayers.addOverlay(bordersLayer, 'borders');
+    controlLayers.addOverlay(bordersLayer, 'Country Borders');
 });
   
 // add plate layer
-d3.json(plates_link, function(data) {
-    // Creating a GeoJSON layer with the retrieved data
-    var plateLayer  = L.geoJson(data, {
-        style: colorPlates,
-        // onEachFeature - popPlate to add popup for each plate
-        onEachFeature: popPlate,
-    }).addTo(map);
-    controlLayers.addOverlay(plateLayer, "Fault Lines");
+//d3.json(plates_link, function(data) {
+//    // Creating a GeoJSON layer with the retrieved data
+//    var plateLayer  = L.geoJson(data, {
+//        style: colorPlates,
+//        // onEachFeature - popPlate to add popup for each plate
+//        onEachFeature: popPlate,
+//    }).addTo(map);
+//    controlLayers.addOverlay(plateLayer, "Fault Lines");
         
 
 
@@ -185,4 +183,4 @@ legend.onAdd = function (map) {
 
 legend.addTo(map);
 
-});
+//});
