d556a440c96021e96f0a8ca16aaab4565eb0b230
filterlesschris <c.benzen@yahoo.com>
2018-03-13 16:23:18 -0700

    updated the styling of the charts

--- dashboard/static/js/line.js
+++ dashboard/static/js/line.js
@@ -3,7 +3,7 @@ var trace1 = {
   y: [10, 15, 13, 17],
   type: 'scatter',
   marker: {
-    color: 'rgb(255, 255, 255)',
+    color: 'rgb(12, 206, 18)',
     size: 12,
     line: {
       color: '#fff',
@@ -17,7 +17,7 @@ var trace2 = {
   y: [16, 5, 11, 9],
   type: 'scatter',
   marker: {
-    color: 'rgb(255, 255, 255)',
+    color: 'rgb(229, 73, 76)',
     size: 12,
     line: {
       color: '#fff',
@@ -30,7 +30,48 @@ var data = [trace1, trace2];
 
 var layout = {
 	paper_bgcolor: '#1a1a1a',
-	plot_bgcolor: '#1a1a1a'
+	plot_bgcolor: '#1a1a1a',
+	annotations:[
+    {
+    xref: 'paper',
+    yref: 'paper',
+    x: -0.2,
+    y: 1.5,
+    xanchor: 'left',
+    yanchor: 'bottom',
+  	text: '{COUNTRY SELECTED} Population Change',
+    font:{
+        family: 'Arial',
+        size: 16,
+        color: 'rgb(255, 255, 255)'
+    },
+    showarrow: false
+    }
+  ],
+  xaxis: {
+    showgrid: true,
+    showline: true,
+    gridcolor: '#bdbdbd',
+    gridwidth: 2,
+    linecolor: '#bdbdbd',
+    tickfont: {
+      family: 'Arial',
+      size: 14,
+      color: '#bdbdbd'
+    }
+  },
+  yaxis: {
+    showgrid: true,
+    showline: true,
+    gridcolor: '#bdbdbd',
+    gridwidth: 2,
+    linecolor: '#bdbdbd',
+    tickfont: {
+      family: 'Arials',
+      size: 14,
+      color: '#bdbdbd'
+    }
+  }
 };
 
 Plotly.newPlot('line-chart', data, layout);
