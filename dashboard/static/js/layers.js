5e1c18f877245ade4287a3e546ba9c72c00193dd
ABFdata <andyfelicitas@gmail.com>
2018-03-14 08:32:47 -0700

    css legend, choropleth js, edit layers js, edit index

--- dashboard/static/js/layers.js
+++ dashboard/static/js/layers.js
@@ -65,6 +65,73 @@ d3.json(borderslink, function(data) {
     controlLayers.addOverlay(bordersLayer, 'Country Borders');
 });
 
+// CHOROPLETH SECTION
+
+var geojson;
+
+// Grabbing data with d3...
+d3.json(borderslink, function(data) {
+
+  // Creating a new choropleth layer
+  geojson = L.choropleth(data, {
+    // Which property in the features to use
+    valueProperty: "name",
+    // Color scale
+    scale: ["#ffffb2", "#b10026"],
+    // Number of breaks in step range
+    steps: 10,
+    // q for quantile, e for equidistant, k for k-means
+    mode: "q",
+    style: {
+      // Border color
+      color: "#fff",
+      weight: 1,
+      fillOpacity: 0.8
+    },
+    // Binding a pop-up to each layer
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("<p class='plate'>" + feature.properties.name + "</p>");
+    }
+  }).addTo(map);
+
+  // Setting up the legend
+  var legend = L.control({ position: "bottomright" });
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+    var limits = geojson.options.limits;
+    var colors = geojson.options.colors;
+    var labels = [];
+
+    // Add min & max
+    var legendInfo = "<h3>Gini Coefficient</h3>" +
+      "<div class=\"labels\">" +
+        "<div class=\"min\">" + limits[0] + "</div>" +
+        "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+      "</div>";
+
+    div.innerHTML = legendInfo;
+
+    limits.forEach(function(limit, index) {
+      labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+    });
+
+    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+    return div;
+  };
+
+  // Adding legend to the map
+  legend.addTo(map);
+
+});
+
+
+
+
+// END CHOROPLETH SECTION
+
+
+
+
 function borderInfo(feature, layer) {
     layer.on({
       click: function(event) {
